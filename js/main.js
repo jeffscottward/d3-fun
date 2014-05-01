452a3709eb9fb9b8d1987ab43fe36ac507fd9eb3
jeff ward <jsward.17@gmail.com>
2014-05-01 10:14:11 -0400

    first commit
    has HTML5BP and basic d3 tutorial

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var dataset = [];
+
+for( var i = 0; i < 50; i++){
+  dataset.push(Math.round(Math.random()*100));  
+}
+
+console.log(dataset);
+
+
+// Select All represnts p tags TO BE CREATED
+d3.select("main").selectAll('div')
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", function(d) {
+    return d*2 + "px";
+  });
